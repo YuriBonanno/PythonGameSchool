cf49ea7cb9602fba01df722342063c32181f9061
YuriBonanno <YuriBonanno@hotmail.com>
2019-03-17 22:43:04 +0100

    FreeSchoolMalware runt nu

--- CodeInIntelliJ/MainLoop.py
+++ CodeInIntelliJ/MainLoop.py
@@ -1,34 +1,39 @@
 import datetime as dT
 import time as rDT
 
+class MainLoop:
 
-def update():
-    pass
+    def update(self):
+        pass
 
-def render():
-    pass
+    def render(self):
+        pass
 
-previous = dT.datetime.now()
-lag = 0
-Frame60 = 0
-lagtotal = 0
-loops = 0
-TimeFrame = dT.datetime.now()
-while True:
-    current = dT.datetime.now()
-    elapsed = current - previous
-    previous = current
-    lag = lag + elapsed.microseconds
-    while lag >= 16667: #frameskip
-        update()
-        lag = lag - 16667
-
-    render()
-    Frame60 = Frame60 + 1
-
-    TimeDiff = dT.datetime.now() - TimeFrame
-    if TimeDiff.seconds >= 1:
-        TimeFrame = dT.datetime.now()
-        print(Frame60)
+    def __init__(self):
+        previous = dT.datetime.now()
+        lag = 0
         Frame60 = 0
-    rDT.sleep(1/60 - lag/1000000)
+        lagtotal = 0
+        loops = 0
+        TimeFrame = dT.datetime.now()
+        while True:
+            current = dT.datetime.now()
+            elapsed = current - previous
+            previous = current
+            lag = lag + elapsed.microseconds
+            while lag >= 16667: #frameskip
+                self.update()
+                lag = lag - 16667
+            self.render()
+            Frame60 = Frame60 + 1
+
+
+            TimeDiff = dT.datetime.now() - TimeFrame
+            if TimeDiff.seconds >= 1:
+                TimeFrame = dT.datetime.now()
+                print(Frame60)
+                Frame60 = 0
+            rDT.sleep(1/60 - lag/1000000)
+
+def main():
+    a = MainLoop()
--- CodeInIntelliJ/StartFileTest.py
+++ CodeInIntelliJ/StartFileTest.py
@@ -1,23 +1,36 @@
 import tkinter as tk
-from tkinter import *
+import MainLoop as mL
 
-root = tk.Tk()
-root.minsize(300, 400)
-root.maxsize(300, 400)
 
-frame1 = tk.Frame(root, width=200, height=200, background="black")
-frame1.configure(height=frame1["height"],width=frame1["width"])
-frame1.grid_propagate(0)
-frame2 = tk.Frame(root, width=50, height=100, background="grey")
 
-#frame1.pack(fill=None, expand=0)
-#frame2.pack(fill=None, expand=0)
-textBox = Label(frame1, text="big dick is in town", fg="white", bg="black")
-frame1.grid(column=0, row=0)
-frame2.grid(column=1, row=0)
-textBox.grid()
 
-print("something")
 
-root.mainloop()
+if __name__ == '__main__':
+    root = tk.Tk()
+    WIDTH = 840
+    HEIGHT = 600
+    root.minsize(WIDTH, HEIGHT)
+    root.maxsize(WIDTH, HEIGHT)
+    frame1 = tk.Frame(root, width=round(WIDTH/3 * 2), height=round(HEIGHT/2), background="black")
+    frame1.configure(height=frame1["height"], width=frame1["width"])
+    frame1.grid_propagate(0)
+    frame2 = tk.Frame(root, width= round(WIDTH/3), height=HEIGHT, background="grey")
+    frame2.configure(height=frame1["height"], width=frame1["width"])
+    frame2.grid_propagate(0)
+    frame3 = tk.Frame(root, width=round(WIDTH/3 * 2), height=round(HEIGHT/2), background="grey")
+    frame3.configure(height=frame1["height"], width=frame1["width"])
+    frame3.grid_propagate(0)
+
+    textBox = tk.Label(frame1, text="big dick is in town", fg="white", bg="black")
+
+    frame1.grid(column=0, row=0)
+    frame2.grid(column=1, row=0)
+    frame3.grid(column=0, row=1)
+    textBox.grid()
+
+    startButton = tk.Button(frame2, text="start", command=mL.main)
+    startButton.pack()
+    print("something")
+
+    root.mainloop()
 

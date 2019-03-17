4bc8c582d858a4f7f59d41eba2e9d6ff2ab22d07
YuriBonanno <YuriBonanno@hotmail.com>
2019-03-17 18:12:42 +0100

    Start to loop and fixed some wndows lol

---
+++ CodeInIntelliJ/MainLoop.py
@@ -0,0 +1,23 @@
+import datetime
+from datetime import *
+
+previous = datetime.now()-datetime.now()
+print(previous)
+# double lag = 0.0;
+# while (true)
+#     {
+#         double current = getCurrentTime();
+# double elapsed = current - previous;
+# previous = current;
+# lag += elapsed;
+#
+# processInput();
+#
+# while (lag >= MS_PER_UPDATE)
+#     {
+#         update();
+#     lag -= MS_PER_UPDATE;
+#     }
+#
+#     render();
+# }
--- CodeInIntelliJ/StartFileTest.py
+++ CodeInIntelliJ/StartFileTest.py
@@ -1,16 +1,23 @@
 import tkinter as tk
+from tkinter import *
 
 root = tk.Tk()
-root.minsize(200, 200)
-root.maxsize(200, 200)
+root.minsize(300, 400)
+root.maxsize(300, 400)
 
-
-#frame1 = tk.Frame(root, width=200, height=200, background="bisque")
-#frame2 = tk.Frame(root, width=50, height = 50, background="#b22222")
+frame1 = tk.Frame(root, width=200, height=200, background="black")
+frame1.configure(height=frame1["height"],width=frame1["width"])
+frame1.grid_propagate(0)
+frame2 = tk.Frame(root, width=50, height=100, background="grey")
 
 #frame1.pack(fill=None, expand=0)
-#frame2.place(relx=.5, rely=.5, anchor="c")
+#frame2.pack(fill=None, expand=0)
+textBox = Label(frame1, text="big dick is in town", fg="white", bg="black")
+frame1.grid(column=0, row=0)
+frame2.grid(column=1, row=0)
+textBox.grid()
+
+print("something")
 
 root.mainloop()
 
-print("something")
